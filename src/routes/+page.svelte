5f3be26856f04e602228a2053603fa4ba9b2b756
tristen <123990328+tristenkurutz@users.noreply.github.com>
2026-01-23 01:36:34 -0500

    some basic cleanup of the nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<div class="flex flex-row gap-6">
-     <h1 class="font-myfont text-6xl leading-tight m-2">
+<div class="flex flex-wrap items-center px-5 md:px-20 lg:px-40 gap-6">
+  <h1 class="font-myfont lg:text-5xl md:text-4xl sm:text-3xl text-2xl leading-tight mt-5">
     TRISTEN<span class="block">KURUTZ</span>
   </h1>
-    <div class="nav flex flex-row gap-8">
-        <button class="">ABOUT</button>
-        <button class="">INDUSTRY EXP.</button>
-        <button class="">PROJECTS</button>
-        <button class="">CONTACT</button>
-    </div>
+
+  <!-- Nav buttons, are on far right for larger screens and are stacked next to the logo for the smaller screens-->
+  <div class="nav flex flex-col sm:flex-row flex-wrap gap-3 sm:gap-5 items-start sm:items-center mt-5 sm:mt-0 sm:ml-auto">
+    <button class="hover:text-blue-700 transition-colors duration-300">ABOUT</button>
+    <button class="hover:text-blue-700 transition-colors duration-300">INDUSTRY EXP.</button>
+    <button class="hover:text-blue-700 transition-colors duration-300">PROJECTS</button>
+    <button class="hover:text-blue-700 transition-colors duration-300">CONTACT</button>
+  </div>
 </div>

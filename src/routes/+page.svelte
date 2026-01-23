30d0622ccab2623815dd1d8220eb12673191fc98
tristen <123990328+tristenkurutz@users.noreply.github.com>
2026-01-23 00:44:36 -0500

    base nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="font-myfont">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-row gap-6">
+     <h1 class="font-myfont text-6xl leading-tight m-2">
+    TRISTEN<span class="block">KURUTZ</span>
+  </h1>
+    <div class="nav flex flex-row gap-8">
+        <button class="">ABOUT</button>
+        <button class="">INDUSTRY EXP.</button>
+        <button class="">PROJECTS</button>
+        <button class="">CONTACT</button>
+    </div>
+</div>

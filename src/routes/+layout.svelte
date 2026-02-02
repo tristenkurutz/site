648ec02efece8f42daaf30494c18ada8ae7027f9
tristen k <cdk6546@rit.edu>
2026-02-02 15:29:57 -0500

    Adding color pallete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,32 @@
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+  <div class="flex flex-wrap items-center px-5 md:px-20 lg:px-40 gap-6 mb-10">
+    <h1
+      class="font-myfont lg:text-5xl md:text-4xl sm:text-3xl text-2xl leading-tight mt-5"
+    >
+      TRISTEN<span class="block">KURUTZ</span>
+    </h1>
+
+    <!-- Nav buttons, are on far right for larger screens and are stacked next to the logo for the smaller screens-->
+    <div
+      class="nav flex flex-col sm:flex-row flex-wrap gap-3 sm:gap-5 items-start sm:items-center mt-5 sm:mt-0 sm:ml-auto"
+    >
+      <button class="hover:text-stone-300 transition-colors duration-300"
+        >ABOUT</button
+      >
+      <button class="hover:text-stone-300 transition-colors duration-300"
+        >INDUSTRY EXP.</button
+      >
+      <button class="hover:text-stone-300 transition-colors duration-300"
+        >PROJECTS</button
+      >
+      <button class="hover:text-stone-300 transition-colors duration-300"
+        >CONTACT</button
+      >
+    </div>
+  </div>
 </svelte:head>
 
-{@render children()}
+<div class="mt-20 px-5 md:px-20 lg:px-40">{@render children()}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{html,js,svelte,ts}'];
+export const content = ["./src/**/*.{html,js,svelte,ts}"];
 export const theme = {
   extend: {
     fontFamily: {
-      myfont: ['GT Walsheim Pro Black', 'sans-serif'],
+      myfont: ["GT Walsheim Pro Black", "sans-serif"],
     },
   },
 };

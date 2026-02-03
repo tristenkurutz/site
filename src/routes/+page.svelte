af20f55f5f250981421d4cf65f1afdee1214d4a9
tristen k <cdk6546@rit.edu>
2026-02-02 21:08:37 -0500

    Header styling + bio added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h2>howdy, i'm tristen!</h2>
+<h2>howdy!</h2>
+<h3>who am i?</h3>
+<p>
+  My name is Tristen, I am a senior Software Engineering major at RIT. I
+  currently specialize in automation and full-stack web development. I have
+  interned for M&T Bank two Summers in a row (2024/2025), and have been a
+  full-stack intern for Innovative Systems (Fall 2025). I am currently interning
+  at Benco Dental as a frontend dev intern on their internal applications team,
+  and will be interning again at M&T Bank this Summer (2026). Currently looking
+  for full-time oppportunities as I graduate in Fall 2026! 👀
+</p>
+
+<h3>coding</h3>

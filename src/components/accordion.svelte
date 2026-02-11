1cfaa409638bc10c71b8601633840873238137d7
tristen k <cdk6546@rit.edu>
2026-02-11 15:28:30 -0500

    working on accordion component for industry exp dropdowns

---
+++ src/components/accordion.svelte
@@ -0,0 +1,7 @@
+<div id="accordion">
+  <div id="accordion-top" class="flex">
+    <div id="accordion-header" class="items-start">title</div>
+    <div id="dropdown-icon" class="ml-auto">icon</div>
+  </div>
+  <div id="accordion-content">content</div>
+</div>

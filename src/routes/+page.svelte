69f0f4c078825f6de03a00d75e7cddce52049d48
tristen k <cdk6546@rit.edu>
2026-02-11 15:29:32 -0500

    moving industry exp to dropdowns

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
+<script>
+  import Accordion from "../components/accordion.svelte";
+</script>
+
 <h2>howdy!</h2>
 <h3>who am i?</h3>
 <p>
-  My name is Tristen, I am a senior Software Engineering major at RIT. I
-  currently specialize in automation and full-stack web development. I have
-  interned for M&T Bank two Summers in a row (2024/2025), and have been a
-  full-stack intern for Innovative Systems (Fall 2025). I am currently interning
-  at Benco Dental as a frontend dev intern on their internal applications team,
-  and will be interning again at M&T Bank this Summer (2026). Currently looking
-  for full-time oppportunities as I graduate in Fall 2026! 👀
+  My name is Tristen, I am a senior Software Engineering major at RIT. [insert
+  some other cool info about me] Currently looking for full-time oppportunities
+  as I graduate in Fall 2026! 👀
 </p>
 
-<h3>coding</h3>
+<h3>industry experience</h3>
+I've had the opportunity to gain hands-on experience with real-world software at various
+companies, including:
+
+<div class="mt-2"><Accordion /></div>
